c660383e429c39148b998240357c9b54237f1ccb
ssearobber <ssearobber@naver.com>
2022-08-03 09:19:42 +0900

    script commit

---
+++ util/commonUtil.js
@@ -0,0 +1,23 @@
+// console.log(
+//   '현재 메모리 사용량(Promise.all 밖) ' +
+//     '\n' +
+//     'rss : ' +
+//     process.memoryUsage().rss / 1024 / 1024 +
+//     'MB' +
+//     '\n' +
+//     'heapTotal : ' +
+//     process.memoryUsage().heapTotal / 1024 / 1024 +
+//     'MB' +
+//     '\n' +
+//     'heapUsed : ' +
+//     process.memoryUsage().heapUsed / 1024 / 1024 +
+//     'MB' +
+//     '\n' +
+//     'external : ' +
+//     process.memoryUsage().external / 1024 / 1024 +
+//     'MB' +
+//     '\n' +
+//     'arrayBuffers : ' +
+//     process.memoryUsage().arrayBuffers / 1024 / 1024 +
+//     'MB',
+// );
